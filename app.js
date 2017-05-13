aded846ed433b8f4ae6d4c880e525a2de29b920c
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-13 16:52:32 +0100

    greeting response mk1

--- app.js
+++ app.js
@@ -369,6 +369,7 @@ function receivedMessage(event) {
     }
   } else if (messageAttachments) {
     getUserInfo(senderID);
+    console.log("getting name now....")
     //moved the below two lines here instead of within getUserInfo function as I want to call that elsewhere without returning this message
     var message = "Hi " + name + ". That's a very nice attachment. Send me some mooooore :)";
     sendTextMessage(senderID, message);
