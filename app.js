c3a3d17182458d4857d28dd598e5dafd55eac267
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-14 17:43:01 +0100

    dictionary

--- app.js
+++ app.js
@@ -584,7 +584,7 @@ function findRhyme(senderID, searchWord) {
     var count = 0;
     for (var i = 0, len = CURRENTDICTIONARY.length; i < len; i++) {
       if(CURRENTDICTIONARY[i].endsWith(ending)){
-        console.log("rhyme found: "+CURRENTDICTIONARY[i]);
+        console.log("rhyme number"+count+" found: "+CURRENTDICTIONARY[i]);
         returnString=returnString+CURRENTDICTIONARY[i]+"\n";
         count++;
       }
