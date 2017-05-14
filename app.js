53f5e93e26a5ab49bf0f162c8ea44407c58a1031
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-14 18:02:39 +0100

    Debugging

--- app.js
+++ app.js
@@ -831,6 +831,7 @@ if (rhymeObject == messageText) {
 
 function checkKeyword(messageText){
 
+console.log(messageText.startsWith("rhyme"));
 //changed this to only search for "rhyme" at the start of the string
  var n = messageText.startsWith("rhyme");
  //this line is being output to the console a lot, often being called twice per instruction - am not sure why???
