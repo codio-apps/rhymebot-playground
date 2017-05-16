66bf5f7befc9c8e49d7f59b20db8378bb2ed9d54
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-16 11:40:20 +0100

    newbie

--- app.js
+++ app.js
@@ -636,8 +636,8 @@ function findRhyme(senderID, searchWord) {
         //if the found word ends in (n)
         if (arrayBin[0].endsWith(")")) {
             console.log("found word "+arrayBin[0]+" with bracket ending, of length : "+arrayBin[0].length+". Fixing it");
-        //    var tmpLen = arrayBin[0].length-3;
-        //    arrayBin = arrayBin[0].slice(tmpLen);
+            var tmpLen = arrayBin[0].length-3;
+            arrayBin = arrayBin[0].slice(tmpLen);
         }
         //save the word to the output array
         RHYMEOUTPUT[found]=arrayBin[0].toLowerCase();
