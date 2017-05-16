c4550b4091af41cca9f71114a37579635c49aee5
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-16 11:39:11 +0100

    newbie

--- app.js
+++ app.js
@@ -632,14 +632,13 @@ function findRhyme(senderID, searchWord) {
       //if the rhyme is a match
       if(CURRENTDICTIONARY[i].endsWith(temp)) {
         arrayBin = CURRENTDICTIONARY[i].split("  ");
-        console.log("splitting line");
 
         //if the found word ends in (n)
-        //if (arrayBin[0].endsWith(")")) {
-        //    console.log("found word "+arrayBin[0]+" with bracket ending, of length : "+arrayBin[0].length+". Fixing it");
+        if (arrayBin[0].endsWith(")")) {
+            console.log("found word "+arrayBin[0]+" with bracket ending, of length : "+arrayBin[0].length+". Fixing it");
         //    var tmpLen = arrayBin[0].length-3;
         //    arrayBin = arrayBin[0].slice(tmpLen);
-        //}
+        }
         //save the word to the output array
         RHYMEOUTPUT[found]=arrayBin[0].toLowerCase();
         console.log("added word: "+RHYMEOUTPUT[found]);
