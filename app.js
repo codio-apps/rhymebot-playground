3bc40d4efc53faabd7125514b2e05d81438f97bb
Adam Stevens <ajstevens@gigz-mbp.home.DHCP.thefacebook.com>
2017-05-18 20:43:17 +0100

    Question

--- app.js
+++ app.js
@@ -542,14 +542,14 @@ function receivedMessage(event) {
 //   db.close();
 // });
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://ajstevens:beatbrothers1!@cluster0-shard-00-00-7fr6a.mongodb.net:27017,cluster0-shard-00-01-7fr6a.mongodb.net:27017,cluster0-shard-00-02-7fr6a.mongodb.net:27017/codio-apps?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://ajstevens:beatbrothers1!@cluster0-shard-00-00-7fr6a.mongodb.net:27017,cluster0-shard-00-01-7fr6a.mongodb.net:27017,cluster0-shard-00-02-7fr6a.mongodb.net:27017/codio-apps?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
 
 
 
