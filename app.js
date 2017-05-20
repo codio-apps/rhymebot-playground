5ced34b873cc1dc36536ca6b4610bf54f63dfe48
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-20 20:07:31 +0100

    chris is back

--- app.js
+++ app.js
@@ -665,8 +665,7 @@ function receivedMessage(event) {
             console.log("Vowel number "+vowelCount+" found at pos: "+nextVowel);
             console.log("phonemeBuffer nextVowel = "+phonemeBuffer[nextVowel]);
             //now stick the rest of the vowels back onto the buffer
-            for (var l = nextVowel, restLen = phonemeBuffer.length; l < restLen, l++){
-
+            for (var l = nextVowel, restLen = phonemeBuffer.length; l < restLen; l++){
               tempString = tempString +phonemeBuffer[l];
             }
             console.log("tempString is "+tempString);
