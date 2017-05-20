b8406992f787c87657192ab8ebd9badd95493d2f
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-20 15:43:17 +0100

    Mongo

--- mongotest.js
+++ mongotest.js
@@ -1,9 +1,17 @@
-// Connect to the MongoDB server
-var MongoClient = require('mongodb').MongoClient;
-console.log("got this far");
-var uri = "mongodb://ajstevens:beatbrothers1!@cluster0-shard-00-00-7fr6a.mongodb.net:27017,cluster0-shard-00-01-7fr6a.mongodb.net:27017,cluster0-shard-00-02-7fr6a.mongodb.net:27017/codio-apps?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-MongoClient.connect(uri, function(err, db) {
-  if (err) throw err;
-  console.log("db created");
-db.close();
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+// Connection URL
+var url = "mongodb://ajstevens:beatbrothers1!@cluster0-shard-00-00-7fr6a.mongodb.net:27017,cluster0-shard-00-01-7fr6a.mongodb.net:27017,cluster0-shard-00-02-7fr6a.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
 });
+
+
+// var url = "mongodb://ajstevens:beatbrothers1!@cluster0-shard-00-00-7fr6a.mongodb.net:27017,cluster0-shard-00-01-7fr6a.mongodb.net:27017,cluster0-shard-00-02-7fr6a.mongodb.net:27017/codio-apps?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
