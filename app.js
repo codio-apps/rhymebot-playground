ba85eb8c10650c554534c7e48d26cc9483e35abd
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-20 17:36:00 +0100

    chris is back

--- app.js
+++ app.js
@@ -641,7 +641,6 @@ function receivedMessage(event) {
   function searchSentence(phonemeString, totalSyllables){
     console.log("searchSentence called on:"+phonemeString);
     var tryer = phonemeString.split("/ "); //array AA1 G Z / EH1 G Z
-    var quickThing =
     tryer[tryer.length-1] = tryer[tryer.length-1].slice(0, quickThing.length-1);
     console.log("quickThing "+tryer[tryer.length-1]);
     var tryerString = "";
