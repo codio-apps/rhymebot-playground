f1446b4fc7c5be2be3b9bb4bd023b42f48ad2fea
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-22 11:07:41 +0100

    chris is back

--- app.js
+++ app.js
@@ -670,11 +670,11 @@ function receivedMessage(event) {
       console.log("vowels are at positions "+vowelPos);
       console.log(wordEndings);
       console.log("longest one is "+wordEndings[wordEndings.length-1]);
-      for (var j = vowelCount-1; j >= 0; j--){
+      for (var j = vowelCount; j >= 0; j--){
           var theWord = getWord(sentenceArray[i]);
           console.log("theWord is "+theWord);
-          var tempString = searchPhonemes(wordEndings[j], theWord, j);
-          console.log("loop j: "+j+" / "+wordEndings[j]+" / "+theWord+" / "+j);
+          var tempString = searchPhonemes(wordEndings[j-1], theWord, j);
+          console.log("loop j: "+j+" / "+wordEndings[j-1]+" / "+theWord);
           console.log(tempString);
       }
     }
