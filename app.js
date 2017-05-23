9764ad95c3fd3c055110642f6635dc71c031d822
Christopher Stevens <christopher.stevens@gmail.com>
2017-05-23 17:29:38 +0100

    chris sorting arrays

--- app.js
+++ app.js
@@ -457,7 +457,7 @@ function receivedMessage(event) {
             if (indexArray[i] != -1) {
               console.log("SearchArray: "+indexArray);
               rhymeArray = searchSentence(indexArray);
-              outputString = outputString+"I found "+rhymeArray[i].length+" rhyme(s) for "+searchArray[i]+"\n"+rhymeArray[i]+"\n";
+              outputString = outputString+"I found "+rhymeArray[i][0].length+" rhyme(s) for "+searchArray[i]+"\n"+rhymeArray[i][0]+"\n";
             }
           }
           messageResponse = outputString;
@@ -627,10 +627,6 @@ function receivedMessage(event) {
         //turn the indexarray back into words, obtain the syllables in this array as well
         outputArray[i] = indexesToWords(indexOutputArray[i], indexArray[i]);
         console.log("Word searching completed OK");
-        console.log("outPutArray i00 "+outputArray[i][0][0]);//words
-        console.log("outPutArray i01 "+outputArray[i][0][1]);//1st rhyme
-        console.log("outPutArray i10 "+outputArray[i][1][0]);//syllables
-        console.log("outPutArray i11 "+outputArray[i][1][1]);//1st syllable
       } else {
         outputArray[i] = ["UNKNOWN"];
       }
@@ -705,7 +701,7 @@ function receivedMessage(event) {
   //function to turn an array of indexes into a more presentable 3d array of words
   //includes duplicate handling and counts syllables as well, putting them into the second array
   function indexesToWords(indexArray, dictionaryIndex){
-    var FINALOUTPUT = new Array();
+    var FINALOUTPUT = [["Words"], ["syllables"]];
     //for every item in the words-that-rhyme array
     //turn them back into words in a new array
     for (var i=0; i<indexArray.length; i++){
